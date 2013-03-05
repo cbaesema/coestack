107d12bed2523746a00b8b6cad5bdef1fd64cb4e
root <root@ubuntu.(none)>
2013-03-05 14:57:03 -0600

    install update

--- install.py
+++ install.py
@@ -16,4 +16,12 @@ print "removing old manifests"
 os.system('rm -rf /etc/puppet/manifests/*')
 print "copy manifests to /etc/puppet/manifests"
 os.system('cp /opt/manifests/manifests/* /etc/puppet/manifests')
+print "copy over site.pp"
+os.system('cp site.pp /etc/puppet/manifests')
+print "load modules"
+os.system('sh /etc/puppet/manifests/puppet_modules.sh')
+print "run first stage"
+os.system('puppet apply -v /etc/puppet/manifests/site.pp')
+print "download plugins"
+os.system('puppet plugin download')
 
